051b999789293612e8e20ad116c18e060d43a2bb
Yiwei Zhang <yw349762@dal.ca>
2018-06-06 18:02:17 -0300

    change to program three

--- program1/program3.py
+++ program1/program3.py
@@ -1,25 +1,48 @@
-import csv
-import json
+# build_index.py
+FILE_URL = "example.csv"
+ES_HOST = {"host": "localhost", "port": 9200}
+INDEX_NAME = 'tweettext'
+TYPE_NAME = '_doc'
+ID_FIELD = 'id'
 
+import csv
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-  print(es)
+# 1. Which libraries do I need ?
+import pandas as pd
+import numpy as np
+import pyes # For documentation around pyes.es : https://pyes.readthedocs.org/en/latest/references/pyes.es.html
+import json
 
 
 
-def csv_reader(file_obj, delimiter=','):
-   reader = csv.reader(file_obj)
-   i = 1
-   results = []
-   for row in reader:
-     print(row)
-     es.index(index='product', doc_type='prod', id=i, body=json.dump([row for row in reader], file_obj))
-     i = i + 1
-     results.append(row)
-     print(row)
+csv_file_object = csv.reader(FILE_URL)
+header = csv_file_object
+header = [item for item in header]
+
+# create ES client, create index
+es = Elasticsearch(hosts=[ES_HOST])
+if es.indices.exists(INDEX_NAME):
+    print("deleting '%s' index..." % (INDEX_NAME))
+    res = es.indices.delete(index=INDEX_NAME)
+    print(" response: '%s'" % (res))
+# since we are running locally, use one shard and no replicas
+request_body = {
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    }
+}
+print("creating '%s' index..." % (INDEX_NAME))
+res = es.indices.create(index=INDEX_NAME, body=request_body)
+print(" response: '%s'" % (res))
 
 
- if __name__ == "__main__":
-  with open("tweettext.csv") as f_obj:
-     csv_reader(f_obj)s.bulk(es, reader, index='tweettext', doc_type='my-type')
+from elasticsearch import helpers, Elasticsearch
+import csv
+
+es = Elasticsearch()
+
+with open('tweettext.csv') as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index='tweettext', doc_type='_doc')
