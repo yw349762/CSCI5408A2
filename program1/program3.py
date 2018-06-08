f2d86f4b9fe1e605d2fea9159e52bc3c64a92372
Yiwei Zhang <yw349762@dal.ca>
2018-06-07 21:08:48 -0300

    change to program three

--- program1/program3.py
+++ program1/program3.py
@@ -16,19 +16,14 @@ header = [item for item in header]
 # create ES client, create index
 es = Elasticsearch(hosts=[ES_HOST])
 if es.indices.exists(INDEX_NAME):
-    print("deleting '%s' index..." % (INDEX_NAME))
     res = es.indices.delete(index=INDEX_NAME)
-    print(" response: '%s'" % (res))
-# since we are running locally, use one shard and no replicas
 request_body = {
     "settings": {
         "number_of_shards": 1,
         "number_of_replicas": 0
     }
 }
-print("creating '%s' index..." % (INDEX_NAME))
 res = es.indices.create(index=INDEX_NAME, body=request_body)
-print(" response: '%s'" % (res))
 
 
 from elasticsearch import helpers, Elasticsearch
